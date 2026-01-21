8a16c13769d37093f71bc497676f75aef66495ae
ayoubMah <ayoubelmahjouby9@gmail.com>
2026-01-22 00:52:14 +0100

    feat: implemented read and write page

--- Phase-1-Storage-Engines-And-Local -Storage/src/Module1_Java_IO/Exercise_1_ThePageAbstraction/Page.java
+++ Phase-1-Storage-Engines-And-Local -Storage/src/Module1_Java_IO/Exercise_1_ThePageAbstraction/Page.java
@@ -3,4 +3,9 @@ package Module1_Java_IO.Exercise_1_ThePageAbstraction;
 public class Page {
     int ID;
     int[] arr = new int[1024]; // 1KB
+    
+    public Page(int ID, int[] arr){
+        this.ID = ID;
+        this.arr = arr;
+    }
 }
--- Phase-1-Storage-Engines-And-Local -Storage/src/Module1_Java_IO/Exercise_1_ThePageAbstraction/PageManager.java
+++ Phase-1-Storage-Engines-And-Local -Storage/src/Module1_Java_IO/Exercise_1_ThePageAbstraction/PageManager.java
@@ -3,6 +3,7 @@ package Module1_Java_IO.Exercise_1_ThePageAbstraction;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.nio.Buffer;
 import java.nio.ByteBuffer;
 
 public class PageManager {
@@ -13,21 +14,44 @@ public class PageManager {
 
     public PageManager() throws FileNotFoundException {}
 
+    // RAM to disk
+    // serialization 
     public void writePage(Page page) throws IOException {
-        long offset = page.ID;
-        offset += page.ID * PAGE_SIZE;
+        long offset = (long)page.ID * PAGE_SIZE;
+
         // step1: create a buffer of the page size
         ByteBuffer buffer = ByteBuffer.allocate(PAGE_SIZE);
         // step2: put the data in
         buffer.putInt(page.ID);
         for (int val : page.arr){
             buffer.putInt(val); // pack the integers one by one
-            file.seek(offset);
-            file.write(val);
         }
-        buffer.array();
+        file.seek(offset);
+        file.write(buffer.array());
     }
+    
+    // disk to RAM
+    // deserialization 
+    public Page readPage(int pageId) throws IOException {
+        long offset = (long)pageId * PAGE_SIZE;
+        file.seek(offset);
+        
+        // the physical container 
+        byte[] arr = new byte[PAGE_SIZE];
+        file.read(arr);
+        
+        ByteBuffer buffer = ByteBuffer.wrap(arr);// don't allocate a new memory, use this existing one as source
 
-    public void readPage(int pageId){
+        // get the header : id
+        int ID = buffer.getInt();
+
+        int[] pageArr = new int[1023]; // 1023 to leave room for the id header
+        for (int i = 0; i < arr.length; i++){  
+            pageArr[i] = buffer.getInt();
+        }
+
+        Page page = new Page(ID, pageArr);
+        return page;
+        
     }
 }
